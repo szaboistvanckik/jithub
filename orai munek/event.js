7aaec2a33fbded664478eb1f55fce25cd0dcdfed
szaboistvanckik <szabo.istvan@ckik.hu>
2024-12-03 10:31:23 +0100

    AHHHHHHHHHHHHHHHHHHH

--- orai munek/event.js
+++ orai munek/event.js
@@ -1,6 +1,22 @@
 function idk() {
-    var szigmaboy = ["Kutya", "cica", "majom", "akjsdkaljsdkjda", "idk", "anyad", "sigmaboy"];
-}
+    var szigmaboy = [
+        "Kutya",
+        "cica", 
+        "majom", 
+        "akjsdkaljsdkjda", 
+        "idk", "anyad", 
+        "sigmaboy"
+    ];
 
 var doboz = document.getElementById("doboz");
 var ul = document.createElement("ul");
+ul.id = "felsorolas";
+doboz.appendChild(ul);
+
+for (var i = 0; index < szigmaboy.length; index++) {
+    var li = document.createElement("li");
+    li.innerText = szigmaboy[i];
+    ul.appendChild(ul) 
+}
+
+}
