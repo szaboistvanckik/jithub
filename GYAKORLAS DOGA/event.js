c8b2204bb0b10f0a0fe667e85ea8ff964a2b752c
szaboistvanckik <szabo.istvan@ckik.hu>
2025-01-07 10:17:10 +0100

    MIT KELL CSNALNI MI A FELADAT

---
+++ GYAKORLAS DOGA/event.js
@@ -0,0 +1,10 @@
+function randomszam(felso, also) {
+    return Math.floor(Math.random()*(felso-also+1)+also);
+}
+
+
+function generalj() {
+    var ertek = parseInt(document.getElementById("x").value);
+    
+
+}
