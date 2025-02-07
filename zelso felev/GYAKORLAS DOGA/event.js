5a7c9cc76dc7ef2f2e1f5f1a0db417f6425a4c54
szaboistvanckik <szabo.istvan@ckik.hu>
2025-02-07 09:37:59 +0100

    híwk tuh

--- zelso felev/GYAKORLAS DOGA/event.js
+++ zelso felev/GYAKORLAS DOGA/event.js
@@ -4,19 +4,16 @@ function randomszam(also, felso) {
 
 function feltoltTomb(x) {
     const lst = [];
-    x = x / 2
-
+    x = x / 2;
     for (let i = 1; i <= x; i++) {
         lst.push(i);
         lst.push(i);
     }
-
     return lst;
 }
 
 function sorSzam(ksz) {
-    i = Math.floor(Math.sqrt(ksz));
-    
+    let i = Math.floor(Math.sqrt(ksz));
     while (ksz % i != 0) {
         i--;
     }
@@ -27,9 +24,7 @@ function shuffle(t) {
     for (let i = 0; i < t.length; i++) {
         const r1 = randomszam(0, t.length - 1);
         const r2 = randomszam(0, t.length - 1);
-        const s = t[r1];
-        t[r1] = t[r2];
-        t[r2] = s;
+        [t[r1], t[r2]] = [t[r2], t[r1]];
     }
     return t;
 }
@@ -38,50 +33,58 @@ function lefed(cella) {
     cella.innerText = "■";
 }
 
-function felfed(lst, i, j, oszlop, cella, info) {
-    cella.innerText = lst[i * oszlop + j]; 
-    info[0]++;
+function felfed(lst, i, j, oszlop, cella, info, pontok, jatekosElem) {
+    cella.innerText = lst[i * oszlop + j];
 
-    if (info[0] % 2 == 0) {
-        if (info[1].innerText != cella.innerText) {
-            setTimeout(function() {
+    if (info.length == 1) {
+        if (info[0].innerText !== cella.innerText) {
+            setTimeout(() => {
+                lefed(info[0]);
                 lefed(cella);
-                lefed(info[1]);
+                jatekosElem.innerText = jatekosElem.innerText.includes("1") ? "2. játékos köre" : "1. játékos köre";
             }, 1000);
+        } else {
+            if (jatekosElem.innerText.includes("1")) {
+                pontok[0].innerText = parseInt(pontok[0].innerText) + 1;
+            } else {
+                pontok[1].innerText = parseInt(pontok[1].innerText) + 1;
+            }
         }
     }
-    else {
-        info[1] = cella;
-    }
 
-    return info;
+    return info.length == 0 ? [cella] : [];
 }
 
-function generalj() { 
-    let info = [0, null];
-
+function generalj() {
     const tabla = document.getElementById("tabla");
     const szam = document.getElementById("x").value * 2;
     const sor = sorSzam(szam);
     const oszlop = szam / sor;
-
     const lst = shuffle(feltoltTomb(szam));
 
-    
+    const pontok = [document.getElementById("pont1"), document.getElementById("pont2")];
+    const jatekosElem = document.getElementById("jatekoskijelzo");
+
     tabla.innerHTML = "";
+    let info = [];
 
     for (let i = 0; i < sor; i++) {
         const tr = document.createElement("tr");
         for (let j = 0; j < oszlop; j++) {
             const td = document.createElement("td");
-            td.addEventListener("click",function() {
-                info = felfed(lst, i, j, oszlop, this, info);
-            })
-
-            // td.innerText= lst[i * oszlop + j];
+            td.addEventListener("click", function() {
+                if (td.innerText === "■") {
+                    info = felfed(lst, i, j, oszlop, td, info, pontok, jatekosElem);
+                }
+            });
             td.innerText = "■";
             tr.appendChild(td);
         }
         tabla.appendChild(tr);
     }
+
+    pontok[0].innerText = "0";
+    pontok[1].innerText = "0";
+    jatekosElem.innerText = "1. játékos köre";
+    
 }
